9e9de3d4976bf2a21398f8b3dd7060903e435f62
AREE93X <angel.enriquez.esp@outlook.com>
2026-09-15 01:32:19 -0600

    Add Pydantic input validation

--- api/main.py
+++ api/main.py
@@ -1,13 +1,110 @@
+
+from typing import Literal
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 
 from src.predict import predict_churn
+
 
 app = FastAPI(
     title="Internet Company Churn Prediction API",
     description="API for predicting customer churn using LightGBM.",
     version="1.0.0"
 )
+
+
+class CustomerData(BaseModel):
+    customerID: str
+
+    gender: Literal[
+        "Male",
+        "Female"
+    ]
+
+    SeniorCitizen: int
+
+    Partner: Literal[
+        "Yes",
+        "No"
+    ]
+
+    Dependents: Literal[
+        "Yes",
+        "No"
+    ]
+
+    BeginDate: str
+
+    MultipleLines: Literal[
+        "Yes",
+        "No",
+        "No phone service"
+    ]
+
+    InternetService: Literal[
+        "DSL",
+        "Fiber optic",
+        "No"
+    ]
+
+    OnlineSecurity: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    OnlineBackup: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    DeviceProtection: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    TechSupport: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    StreamingTV: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    StreamingMovies: Literal[
+        "Yes",
+        "No",
+        "No internet service"
+    ]
+
+    Type: Literal[
+        "Month-to-month",
+        "One year",
+        "Two year"
+    ]
+
+    PaperlessBilling: Literal[
+        "Yes",
+        "No"
+    ]
+
+    PaymentMethod: Literal[
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+
+    MonthlyCharges: float
+    TotalCharges: str
 
 
 @app.get("/")
@@ -19,8 +116,11 @@ def root():
 
 
 @app.post("/predict")
-def predict(data: dict):
-    df = pd.DataFrame([data])
+def predict(data: CustomerData):
+
+    df = pd.DataFrame([
+        data.model_dump()
+    ])
 
     results = predict_churn(df)
 
@@ -32,3 +132,4 @@ def predict(data: dict):
             results["Probabilidad_churn"].iloc[0]
         )
     }
+
